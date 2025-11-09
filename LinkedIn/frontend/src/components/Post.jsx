a8d80002c31a1d5678e1bc96927d2b94c6fe5067
Swayam Mhaskar <130598178+BMOBOX@users.noreply.github.com>
2025-11-09 11:38:25 +0530

    Update Post.jsx

--- LinkedIn/frontend/src/components/Post.jsx
+++ LinkedIn/frontend/src/components/Post.jsx
@@ -11,7 +11,7 @@ import { LuSendHorizontal } from "react-icons/lu";
 import { io } from "socket.io-client";
 import ConnectionButton from "./ConnectionButton";
 
-let socket = io(serverUrl);
+let socket = io(`${serverUrl}`);
 function Post({ id, author, like, comment, description, image, createdAt }) {
   let [more, setMore] = useState(false);
   let { serverUrl } = useContext(authDataContext);
